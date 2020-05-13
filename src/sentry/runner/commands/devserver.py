551bc296485bd3ff9f88bbf71b0d28b212b09596
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-05-13 13:54:06 -0700

    feat(dev): Make devserver output great again
     - Color prefix with a bar to clearly see separation
     - Add status code colors, with padding to align URLs
     - Highlight uwsgi server reboots
     - Highlight stacktrace errors

--- src/sentry/runner/commands/devserver.py
+++ src/sentry/runner/commands/devserver.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function
 
 import click
 import six
+import types
 from six.moves.urllib.parse import urlparse
 
 from sentry.runner.decorators import configuration, log_options
@@ -36,6 +37,9 @@ def _get_daemon(name):
     "--prefix/--no-prefix", default=True, help="Show the service name prefix and timestamp"
 )
 @click.option(
+    "--pretty/--no-pretty", default=True, help="Styleize various outputs from the devserver"
+)
+@click.option(
     "--styleguide/--no-styleguide",
     default=False,
     help="Start local styleguide web server on port 9001",
@@ -60,7 +64,16 @@ def _get_daemon(name):
 @log_options()
 @configuration
 def devserver(
-    reload, watchers, workers, experimental_spa, styleguide, prefix, environment, skip_daemons, bind
+    reload,
+    watchers,
+    workers,
+    experimental_spa,
+    styleguide,
+    prefix,
+    pretty,
+    environment,
+    skip_daemons,
+    bind,
 ):
     "Starts a lightweight web server for development."
     skip_daemons = set(skip_daemons.split(",")) if skip_daemons else set()
@@ -226,9 +239,9 @@ def devserver(
     # A better log-format for local dev when running through honcho,
     # but if there aren't any other daemons, we don't want to override.
     if daemons:
-        uwsgi_overrides["log-format"] = '"%(method) %(status) %(uri) %(proto)" %(size)'
+        uwsgi_overrides["log-format"] = "%(method) %(status) %(uri) %(proto) %(size)"
     else:
-        uwsgi_overrides["log-format"] = '[%(ltime)] "%(method) %(status) %(uri) %(proto)" %(size)'
+        uwsgi_overrides["log-format"] = "[%(ltime)] %(method) %(status) %(uri) %(proto) %(size)"
 
     server = SentryHTTPServer(host=host, port=port, workers=1, extra_options=uwsgi_overrides)
 
@@ -254,7 +267,14 @@ def devserver(
 
     cwd = os.path.realpath(os.path.join(settings.PROJECT_ROOT, os.pardir, os.pardir))
 
-    manager = Manager(Printer(prefix=prefix))
+    honcho_printer = Printer(prefix=prefix)
+
+    if pretty:
+        from sentry.runner.formatting import monkeypatch_honcho_write
+
+        honcho_printer.write = types.MethodType(monkeypatch_honcho_write, honcho_printer)
+
+    manager = Manager(honcho_printer)
     for name, cmd in daemons:
         if name not in skip_daemons:
             manager.add_process(name, list2cmdline(cmd), quiet=False, cwd=cwd)
